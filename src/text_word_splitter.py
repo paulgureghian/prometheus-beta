582a804a19366b3d6597869c5574f465b0cddf94
paulgureghian <paulgureghian@users.noreply.github.com>
2025-02-23 21:25:26 +0000

    Final implementation with precise word splitting logic

--- src/text_word_splitter.py
+++ src/text_word_splitter.py
@@ -22,59 +22,32 @@ def split_text_to_words(text):
     if not text:
         return []
     
-    # Complex regex-based strategy
-    def advanced_split(s):
-        # Regex to split on capital letters while preserving context
-        words = []
-        i = 0
-        while i < len(s):
-            # Detect quote sequences
-            if s[i] in '"\'':
-                quote_end = i
-                while quote_end + 1 < len(s) and s[quote_end + 1] in '"\'':
-                    quote_end += 1
-                quote_segment = s[i:quote_end+1]
-                
-                # If we're in a quote segment, treat it specially
-                if len(words) > 0 and '"' in words[-1]:
-                    words[-1] += quote_segment
-                else:
-                    words.append(quote_segment)
-                i = quote_end + 1
-                continue
-            
-            # Handle uppercase sequences
-            if s[i].isupper():
-                if len(words) > 0 and words[-1].isupper() and len(words[-1]) == 1:
-                    words[-1] += s[i]
-                else:
-                    words.append(s[i])
-                i += 1
-                continue
-            
-            # Detect word fragments
-            word_fragment = ""
-            start_fragment = i
-            
-            # Accumulate characters in the fragment
-            while i < len(s) and (not s[i].isupper() or 
-                                  (len(word_fragment) > 0 and word_fragment[-1].islower())):
-                # Special handling for punctuation
-                if not s[i].isalnum() and s[i] not in '"\'':
-                    if word_fragment and word_fragment[-1] != s[i]:
-                        break
-                
-                word_fragment += s[i]
-                i += 1
-            
-            # Punctuation handling
-            while i < len(s) and not s[i].isalnum() and s[i] not in '"\'':
-                word_fragment += s[i]
-                i += 1
-            
-            if word_fragment:
-                words.append(word_fragment)
-        
-        return words
+    words = []
+    current_word = text[0]
     
-    return advanced_split(text)
+    for char in text[1:]:
+        if char.isupper() and not current_word[-1].isupper():
+            # Split on capital letter transition from lowercase
+            words.append(current_word)
+            current_word = char
+        elif not char.isalnum() and char not in '"\'':
+            # Punctuation handling
+            if current_word and current_word[-1].isupper() and len(current_word) == 1:
+                # Extended uppercase sequence
+                current_word += char
+            else:
+                # Start a new punctuation-based word
+                if current_word:
+                    words.append(current_word)
+                current_word = char
+        elif char.isupper() and current_word.isupper() and len(current_word) == 1:
+            # Continue uppercase sequence
+            current_word += char
+        else:
+            current_word += char
+    
+    # Append final word
+    if current_word:
+        words.append(current_word)
+    
+    return words
